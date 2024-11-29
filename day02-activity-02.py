af8f26f97adbeba79de15d4fcea63172da5b8659
Roberto Albanesi <beto0607@gmail.com>
2024-11-29 22:38:20 +0100

    Initial commit + day 01 and 02

---
+++ day02-activity-02.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+from machine import Pin
+import time
+
+
+red = Pin(18, Pin.OUT)
+green = Pin(19, Pin.OUT)
+amber = Pin(20, Pin.OUT)
+
+
+counter = 1
+while counter < 11:
+    red.value(1)
+    amber.value(1)
+    green.value(1)
+
+    time.sleep(0.5)
+
+    red.value(0)
+    amber.value(0)
+    green.value(0)
+    time.sleep(0.5)
+    counter += 1
